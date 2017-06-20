307f9e063e1b0b58389ca73d9e85be60754c98f0
Jan Krcmar <honza801@gmail.com>
2017-06-20 11:28:41 +0200

    file upload logic moved to dedicated file. now used also in uploadform
    switched from $.ajax to xhr

---
+++ ajax/libs/pleasewaitdialog.js
@@ -0,0 +1,54 @@
+// https://css-tricks.com/drag-and-drop-file-uploading/
+function handleUpload(file) {
+    var xhr = new XMLHttpRequest();
+    form = $('#dropzone').get(0);
+    var formData = new FormData(form);
+    if (file != null) {
+        formData.append('file', file);
+    }
+    
+    xhr.upload.addEventListener('progress', function(event) {
+        var percent = (100 * event.loaded / event.total);
+        $(".progress-bar").text(Math.round(percent) + "%");
+    });
+    xhr.addEventListener('readystatechange', function(event) {
+        if (event.target.readyState == 4 && event.target.responseText) {
+            // we got a response from the server and we're replacing the whole
+            // current document content with it, simulating a page reload
+            var newDocument = document.open('text/html', 'replace');
+            newDocument.write(event.target.responseText);
+            newDocument.close();
+        } else {
+            //throw new Error('Error in the response.');
+        }
+    });
+    action = form.getAttribute('action') ? form.getAttribute('action'):"";
+    xhr.open(form.getAttribute('method'), action, true);
+    xhr.send(formData);
+}
+
+$(document).ready(function () {
+    $(document).on('drop', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+    $(document).on('dragover', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+    $('#dropzone').on('drop', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        showPleaseWaitDialog();
+        files = e.originalEvent.dataTransfer.files;
+        $.each(files, function(index, file) {
+            handleUpload(file);
+        });
+    });
+    $('#dropzone').on('submit', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        showPleaseWaitDialog();
+        handleUpload(null);
+    });
+});
